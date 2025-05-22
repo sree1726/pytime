3b9d522c44cfe8ca435f6bd85a829b8279ffcc35
admin <admin@faas.com>
2025-05-22 09:50:04 +0530

    first commit

---
+++ func.py
@@ -0,0 +1,25 @@
+from parliament import Context, event
+
+
+@event
+def main(context: Context):
+    """
+    Function template
+    The context parameter contains the Flask request object and any
+    CloudEvent received with the request.
+    """
+
+    # Print current date and time
+    print("Current date and time:", datetime.now())
+
+    # Print the entire cloud_event
+    print("CloudEvent received:")
+    print(context.cloud_event)
+
+    # Optional: print specific fields
+    print("CloudEvent data:")
+    print(context.cloud_event.data)
+
+    # The return value here will be applied as the data attribute
+    # of a CloudEvent returned to the function invoker
+    return context.cloud_event.data
